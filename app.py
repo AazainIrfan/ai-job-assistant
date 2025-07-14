aa90c0bffa970a1fcdf8dda8da07849791b2bb33
Muhammad Aazain Irfan <irfan2mx@dukes.jmu.edu>
2025-07-14 01:32:40 -0400

    google verification added

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ st.set_page_config(
     layout="wide"
 )
 
+# NEW: Add Google verification meta tag
+st.markdown('<meta name="google-site-verification" content="<meta name="google-site-verification" content="pEQapcdTkxkhy-qVo5nGZZzzhH71WfiK-k3fXiNC2tw" />" />', unsafe_allow_html=True)
+
+
 # --- Custom CSS ---
 st.markdown("""
 <style>
